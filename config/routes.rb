032bfba342fc84b00d43208973157d7d93491654
Myles Lewis Page <68628500+Myles-Lewis-Page@users.noreply.github.com>
2022-11-14 11:12:31 -0700

    updated config information

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   get "/trails", to: "pages#trails"
   get "/account", to: "pages#account"
   
+  get "/gardenofthegods", to: "pages#garden"
+  get "/sandcanyon", to: "pages#sand"
+  get "/ouray", to: "pages#ouray"
+  get "/ice", to: "pages#ice"
+  get "/manitou", to: "pages#manitou"
+  
 end
