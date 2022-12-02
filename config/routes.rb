94450c53d7fdca97d5ddb9ecca526de9f2c73006
Myles Lewis Page <68628500+Myles-Lewis-Page@users.noreply.github.com>
2022-12-02 15:35:08 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,17 @@ Rails.application.routes.draw do
   get "/ouray", to: "pages#ouray"
   get "/ice", to: "pages#ice"
   get "/manitou", to: "pages#manitou"
+  
+  get "/emerald", to: "pages#emerald"
+  get "/stmary", to: "pages#stmary"
+  get "/bierstadt", to: "pages#bierstadt"
+  get "/quandary", to: "pages#quandary"
+  get "/high", to: "pages#high"
+  
+  get "/maroon", to: "pages#maroon"
+  get "/independence", to: "pages#independence"
+  get "/flatiron", to: "pages#flatiron"
+  get "/hanging", to: "pages#hanging"
+  get "/pulpit", to: "pages#pulpit"
 
 end
