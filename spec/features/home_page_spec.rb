cb9d9caecea786d9294200d3de1411231fba749e
devinhall114 <99219403+devinhall114@users.noreply.github.com>
2022-12-02 16:54:16 -0700

    Update home_page_spec.rb

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 
   RSpec.feature "Visiting the homepage", type: :feature do
     scenario "The visitor should see trails" do
-      visit trails_path
       expect(page).to have_text("trails")
     end
   end
