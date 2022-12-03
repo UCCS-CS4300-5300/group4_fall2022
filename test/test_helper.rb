06c2deaf37237807fa487de14aae4a211dcc1a28
Prakash0030 <spathuru@uccs.edu>
2022-12-03 00:29:33 +0000

    simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'simplecov'
+SimpleCov.start
+
+# Previous content of test helper now starts here
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
